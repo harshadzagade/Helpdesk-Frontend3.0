808a21a86f2b0299c3f45f9c637f1401c4af4c09
harshadzagade <hzagade21@gmail.com>
2026-08-29 12:34:46 +0530

    Allow hello met host in Vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true, // Listen on all network interfaces'
-    allowedHosts: [ 'localhost']
+    allowedHosts: ['localhost', 'hello.met.edu']
   },
   plugins: [react()],
 })
